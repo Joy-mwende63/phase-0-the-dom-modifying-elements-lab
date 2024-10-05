e9b2298694b8677ae6ef6b6de9d687e7bc029a1f
Joy-mwende63 <mwendejoy47@gmail.com>
2024-10-05 15:11:31 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+console.log("Script is running");
+
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory'
+newHeader.textContent = "Your Header Text";
+newHeader.textContent = "JOY is the champion"
+document.body.appendChild(newHeader);
+console.log(newHeader);
